d9b435d2fd8373c8fe824590782766b8f599ba2b
ezequielNavarrete <ezequieldario004@gmail.com>
2022-01-08 21:08:27 -0300

    agregada carpeta de primer curso

---
+++ Course1/tp2/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/golang-coursera/Course1/tp2/exercises"
+)
+
+func main() {
+	exercises.Findian()
+	//exercises.Trunc()
+}
---
+++ Course1/tp4/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/golang-coursera/Course1/tp4/exercises"
+)
+
+func main() {
+	exercises.MakeJson()
+	exercises.Read()
+}
